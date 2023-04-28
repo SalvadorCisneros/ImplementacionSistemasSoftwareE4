3c4a0947002a0cd1722c7b23f34a74c9812ba03d
Salvador Cisneros <77074008+SalvadorCisneros@users.noreply.github.com>
2023-04-28 16:26:28 -0600

    Conexion de datos MainTable con el API

--- login-page/src/components/mainTable/mainTable.jsx
+++ login-page/src/components/mainTable/mainTable.jsx
@@ -1,46 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import "./mainTable.css"
 import { useNavigate } from "react-router-dom";
 
-fetch('http://localhost:5000/datospersonales')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
-
-
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'id_usuario', headerName: 'ID', width: 70 },
+  { field: 'nombre', headerName: 'Nombre', width: 130 },
+  { field: 'apellido', headerName: 'Apellido', width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'antigüedad',
+    headerName: 'Antiguedad',
+    type: 'char',
+    width: 130,
+  },
+  {
+    field: 'edad',
+    headerName: 'Edad',
     type: 'number',
     width: 90,
   },
   {
-    field: 'tel',
+    field: 'direccion',
+    headerName: 'Direccion',
+    type: 'char',
+    width: 130,
+  },
+  {
+    field: 'estudio',
+    headerName: 'Estudios',
+    type: 'char',
+    width: 130,
+  },
+  {
+    field: 'telefono',
     headerName: 'Telephone',
     type: 'char',
     width: 130,
   },
   {
-    field: 'email',
-    headerName: 'Correo',
+    field: 'universidad',
+    headerName: 'Universidad',
     type: 'char',
     width: 130,
   },
+  {
+    field: 'estructura_3',
+    headerName: 'Estructura3',
+    type: 'char',
+    width: 130,
+  },
+  {
+    field: 'estructura_4',
+    headerName: 'Estructura2',
+    type: 'char',
+    width: 130,
+  },
+  {
+    field: 'estructura_5',
+    headerName: 'Estructura3',
+    type: 'char',
+    width: 130,
+  },
+
   {
     field: 'fullName',
     headerName: 'Full name',
@@ -48,63 +69,58 @@ const columns = [
     sortable: false,
     width: 160,
     valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      `${params.row.nombre|| ''} ${params.row.apellido || ''}`,
   },
 ];
 
-
-
-export const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, tel: 8126433820, email: 'jon@ternium.com.mx' },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, tel: 8126433820, email: 'Cersei@ternium.com.mx' },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45, tel: 8143543534, email: 'Jaime@ternium.com.mx' },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16, tel: 8199255412, email: 'Arya@ternium.com.mx' },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null, tel: 8126433820, email: 'Daenerys@ternium.com.mx' },
-  { id: 6, lastName: 'Melisandre', firstName: 'Luis', age: 150, tel: 8121544524, email: 'Luis@ternium.com.mx'},
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44, tel: 8184455212, email: 'Ferrara@ternium.com.mx' },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36, tel: 8112544114, email: 'Rossini@ternium.com.mx' },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 6, tel: 8153453453, email: 'Harvey@ternium.com.mx' },
-];
-
-
-export let activeID
- 
-
 export default function DataTable() {
   const navigate = useNavigate();
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/datospersonales')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setRows(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
+  console.log(rows)
+ 
   const [selectionModel, setSelectionModel] = React.useState([]);
-  
+
   return (
     
-    <div className='tabla' style={{ height: 600, width: '100%' }}>
-      <DataGrid
-       
-        rows={rows} 
-        
-        
+    <div className='tabla' style={{ height: 600, width: '100%' }} >
+      
+      <DataGrid getRowId={(row) => row.id_usuario}
+      
+        rows={rows}
         columns={columns}
         pageSize={5}
-        rowsPerPageOptions={[5]}  
+        rowsPerPageOptions={[5]}
         checkboxSelection
         onSelectionModelChange={(newSelection) => {
           setSelectionModel(newSelection.selectionModel);
         }}
         selectionModel={selectionModel}
-        
-        
-        
-        onRowDoubleClick={(params) => navigate("/profile" , 
-        {state:
-          {
-
-          firstName:params.row.firstName, 
-          lastName:params.row.lastName,
-          age:params.row.age, 
-          tel:params.row.tel,
-          email:params.row.email
-        
-        }})}
-        
+        onRowDoubleClick={(params) => navigate("/profile", {
+          state: {
+            firstName: params.row.firstName,
+            lastName: params.row.lastName,
+            age: params.row.age,
+            tel: params.row.tel,
+            email: params.row.email
+          }
+        })}
       />
       {selectionModel.map((val) => (
         <h1>{val}</h1>
@@ -112,5 +128,3 @@ export default function DataTable() {
     </div>
   );
 }
-
-
